c1a6a0ee107cfeea483e1a925451b3a10f98eaa5
ElKariboosama <tsunaris123456789@gmail.com>
2024-02-19 15:50:20 +0100

    added random problem generation

--- app/src/main/java/fr/mathis150/maths_ranked/Problem.java
+++ app/src/main/java/fr/mathis150/maths_ranked/Problem.java
@@ -1,5 +1,7 @@
 package fr.mathis150.maths_ranked;
 
+import android.annotation.SuppressLint;
+
 import java.util.Objects;
 import java.util.Random;
 
@@ -17,11 +19,30 @@ public class Problem {
         return Objects.equals(attempt, this.answer);
     }
 
-    Problem generate(int seed,int n) {
-
+    @SuppressLint("DefaultLocale")
+    Problem generateRandomProblem(int seed) {
+        int difficulty = 10;
         Random generator = new Random(seed);
-        for (int i = 0;i < n; i++) {
+        int problemkind = generator.nextInt() % 4;
 
+        String equation = "";
+        int initial = generator.nextInt() % difficulty + 1;
+        int mult = generator.nextInt() % difficulty + 2;
+        int divide = generator.nextInt() % difficulty + 2;
+        int sub = generator.nextInt() % difficulty + 1;
+        int add = generator.nextInt() % difficulty + 1;
+
+        switch (problemkind) {
+            case 0:
+                equation = String.format("%s%d%s%d%s%d%s%d","(",mult,"x + ",add,") / ",divide," = ",((mult*initial) + add) / divide);
+            case 1:
+                equation = String.format("%d%s%d%s%d%s%d",mult,"x/",divide," + ",add,") = ",((mult*initial)/divide) + add);
+            case 2:
+                equation = String.format("%d%s%d%s%d%s%d",mult,"(x + ",add,") - ",sub," = ",((initial + add) * mult ) - sub);
+            case 3:
+                equation = String.format("%s%d%s%d%s%d%s%d","(x - ", sub,") / ",divide," + ",add," = ",((initial - sub)/divide)+add);
         }
+
+        return new Problem(equation,String.valueOf(initial));
     }
 }
