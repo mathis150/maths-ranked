caf87b24fdd6a23d62a84b2663a3f235402f0a1e
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-02-22 16:00:43 +0100

    Generates fragments

---
+++ app/src/main/java/fr/mathis150/maths_ranked/OnLoad.java
@@ -0,0 +1,14 @@
+package fr.mathis150.maths_ranked;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnLoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_load);
+    }
+}
