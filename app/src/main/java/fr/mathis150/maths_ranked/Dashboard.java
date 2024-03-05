324ec265bc52ad956c5574d1444a64a785b739e9
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-03-05 16:28:23 +0100

    UPDATE FRONTEND PAGES

--- app/src/main/java/fr/mathis150/maths_ranked/Dashboard.java
+++ app/src/main/java/fr/mathis150/maths_ranked/Dashboard.java
@@ -1,14 +1,78 @@
 package fr.mathis150.maths_ranked;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
-public class Dashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
+        NavigationView navigationView = findViewById(R.id.contentNavbar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new Home()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.nav_home){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new Home()).commit();
+        }
+        if(item.getItemId() == R.id.nav_about){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new AboutUs()).commit();
+        }
+        if(item.getItemId() == R.id.nav_logout){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new Logout()).commit();
+        }
+        if(item.getItemId() == R.id.nav_staff){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, new StaffList()).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            getOnBackPressedDispatcher().onBackPressed();
+        }
     }
 }
--- app/src/main/java/fr/mathis150/maths_ranked/MainActivity.java
+++ app/src/main/java/fr/mathis150/maths_ranked/MainActivity.java
@@ -1,8 +1,15 @@
 package fr.mathis150.maths_ranked;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Connection connection = new Connection();
 
+        registerFragment(new LoginPage(this));
+    }
 
+    public void fragmentLogin(View v) {
+        registerFragment(new LoginPage(this));
+    }
+
+    public void fragmentRegister(View v) {
+        registerFragment(new RegisterPage(this));
+    }
+
+    public void registerFragment(Fragment f) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, f);
+        fragmentTransaction.commit();
+    }
+    public void onLogged() {
+        Intent switchActivityIntent = new Intent(this, Dashboard.class);
+        startActivity(switchActivityIntent);
     }
 }
